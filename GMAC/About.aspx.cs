c2911fc19dd996fd289b54c96cf7eaa17cc2ab66
subodh.chowdary6 <subodh.chowdary6@gmail.com>
2019-12-05 12:13:17 +0530

    Changes for vulnerabilities

--- GMAC/About.aspx.cs
+++ GMAC/About.aspx.cs
@@ -9,11 +9,11 @@ namespace GMAC
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            string name = request.querystring["name"];
+            Response.Write("Hello " + name); // Noncompliant
             //string name = Request.QueryString["name"];
-            //Response.Write("Hello " + name); // Noncompliant
-            string name = Request.QueryString["name"];
-            name = System.Web.Security.AntiXss.AntiXssEncoder.HtmlEncode(name, true);
-            Response.Write("Hello " + name);
+            //name = System.Web.Security.AntiXss.AntiXssEncoder.HtmlEncode(name, true);
+            //Response.Write("Hello " + name);
 
         }
     }

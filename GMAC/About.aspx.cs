202104359320a251b6265ea0704b6e05a6347f0b
subodh.chowdary6 <subodh.chowdary6@gmail.com>
2019-12-05 12:16:24 +0530

    Changes error

--- GMAC/About.aspx.cs
+++ GMAC/About.aspx.cs
@@ -9,7 +9,7 @@ namespace GMAC
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            string name = request.querystring["name"];
+            string name = Request.QueryString["name"];
             Response.Write("Hello " + name); // Noncompliant
             //string name = Request.QueryString["name"];
             //name = System.Web.Security.AntiXss.AntiXssEncoder.HtmlEncode(name, true);
